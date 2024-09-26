b06f50e7e091598811bfa4f859a28a686c0327ba
Kostya Naida <varkac69@gmail.com>
2024-09-26 10:29:35 +0300

    Added about and contacts components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,62 @@
+'use client'
+
+import Link from 'next/link'
 import styles from './Header.module.css'
+import { usePathname } from 'next/navigation'
 
 export default function Header() {
-	return <div className={styles.header}>Header</div>
+	const pathName = usePathname()
+	return (
+		<header className={styles.header}>
+			<div className={styles.logo}>
+				<Link href="/">Encashment</Link>
+			</div>
+			<div>
+				<nav>
+					<ul className={styles.navList}>
+						<li>
+							<Link
+								href="/"
+								className={
+									pathName === '/'
+										? `${styles.active} ${styles.navLink}`
+										: styles.navLink
+								}
+							>
+								Головна
+							</Link>
+						</li>
+						<li>
+							<Link
+								href="/about"
+								className={
+									pathName === '/about'
+										? `${styles.active} ${styles.navLink}`
+										: styles.navLink
+								}
+							>
+								Про нас
+							</Link>
+						</li>
+						<li>
+							<Link
+								href="/contacts"
+								className={
+									pathName === '/contacts'
+										? `${styles.active} ${styles.navLink}`
+										: styles.navLink
+								}
+							>
+								Контакти
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div>
+				<Link href="/login">Вхід</Link>
+				<Link href="/register">Реєстрація</Link>
+			</div>
+		</header>
+	)
 }
